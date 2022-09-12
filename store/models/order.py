43b0b9e45da64e184622e21c77228a9cb0c46ec4
khasanovmma <khasanovmma010@gmail.com>
2022-09-12 14:22:18 +0500

    refact order model

--- store/models/order.py
+++ store/models/order.py
@@ -8,6 +8,7 @@ class Order(CreatedAtAbstract):
     items = models.ManyToManyField("store.Item", related_name='items', blank=True)
     ordered_by =  models.CharField(max_length=120, null=True)
     email = models.EmailField(null=True)
+    session_id = models.TextField(null=True)
     
     def __str__(self):
         return f"{self.id} - order"
